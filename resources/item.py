7fdb7458a8dadbaaf813acee959a8e8ae969be14
Mrunal <msalvi96@stevens.edu>
2019-05-27 17:48:13 -0400

    created store model

--- resources/item.py
+++ resources/item.py
@@ -10,6 +10,11 @@ class Item(Resource):
         required=True,
         help="This field cannot be left blank!",
     )
+    parser.add_argument('store_id',
+        type=int,
+        required=True,
+        help="Every Item needs a Store ID!",
+    )
 
     @jwt_required()
     def get(self, name):
@@ -29,7 +34,7 @@ class Item(Resource):
 
 
         data = Item.parser.parse_args()
-        item = ItemModel(name, data['price'])
+        item = ItemModel(name, data['price'], data['store_id'])
         
         try:
             ItemModel.save_to_db(item)
@@ -60,7 +65,7 @@ class Item(Resource):
         item = ItemModel.find_by_name(name)
 
         if item is None:
-            item = ItemModel(name, data['price'])
+            item = ItemModel(name, data['price'], data['store_id'])
         else:
             item.price = data['price']
 
